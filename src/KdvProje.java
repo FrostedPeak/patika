ff2ab4d8c2dfec6874ccc7890998230fa4aa0bd0
FrostedPeak <lidertakimi@gmail.com>
2023-05-18 18:16:25 +0300

    2. test

--- src/KdvProje.java
+++ src/KdvProje.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class KdvProje {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double kdvsizTutar,kdvliTutar,kdvOranı,kdvMiktarı;
+        double kdvsizTutar,kdvliTutar,kdvOrani,kdvMiktari;
         System.out.print("Para miktarını giriniz : ");
         kdvsizTutar = input.nextDouble();
-        kdvOranı = kdvsizTutar > 1000 ? 0.08 : 0.18;
-        kdvMiktarı = kdvsizTutar * kdvOranı;
-        kdvliTutar = kdvsizTutar + kdvMiktarı;
-        String bilgi = kdvOranı == 0.08 ? "Girdiğiniz para miktarı 1000'den büyük olduğu için KDV oranınız %8 olarak alınacaktır." : "Girdiğiniz para miktarı 1000'den küçük olduğu için KDV oranınız %18 olarak alınacaktır.";
+        kdvOrani = kdvsizTutar > 1000 ? 0.08 : 0.18;
+        kdvMiktari = kdvsizTutar * kdvOrani;
+        kdvliTutar = kdvsizTutar + kdvMiktari;
+        String bilgi = kdvOrani == 0.08 ? "Girdiğiniz para miktarı 1000'den büyük olduğu için KDV oranınız %8 olarak alınacaktır." : "Girdiğiniz para miktarı 1000'den küçük olduğu için KDV oranınız %18 olarak alınacaktır.";
         System.out.println(bilgi);
         System.out.println("KDV'siz Fiyat : " + kdvsizTutar);
         System.out.println("KDV'li Fiyat : " + kdvliTutar);
-        System.out.print("KDV Tutarı : " + kdvMiktarı);
+        System.out.print("KDV Tutarı : " + kdvMiktari);
     }
 }
