fd0214d4646dcaae2872ab354969b49b45f7087c
FrostedPeak <lidertakimi@gmail.com>
2023-05-23 00:39:00 +0300

    2. test

--- src/teksayiproje.java
+++ src/teksayiproje.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class teksayiproje {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int sayi = 0, sum = 0;
-        while (sayi%2 == 0) {
-            System.out.print("Bir sayi giriniz : ");
-            sayi = input.nextInt();
-            if (sayi%4 == 0) {
-                sum += sayi;
-            }
+
+        System.out.print("Bir sayi giriniz : ");
+        int sayi = input.nextInt();
+
+        for (int i = 1; i <= sayi; i *= 4) {
+            System.out.println("4'ün katları : " + i);
         }
-        System.out.println("Toplam : " + sum);
+        for (int i = 1; i <= sayi; i *= 5) {
+            System.out.println("5'in katları : " + i);
+        }
     }
 }
